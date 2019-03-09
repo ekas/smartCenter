2216a992a37caaee079e1dcf4f81b77b6edc171f
ekas <ekaspreet93.singh@gmail.com>
2019-03-10 04:24:03 +0530

    Added Dashboard data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,78 +1,47 @@
 <template>
-  <v-container
-    fill-height
-    fluid
-    grid-list-xl
-  >
+  <v-container fill-height fluid grid-list-xl>
     <v-layout wrap>
-      <v-flex
-        sm6
-        xs12
-        md6
-        lg3
-      >
+      <v-flex sm6 xs12 md6 lg4>
         <material-stats-card
           color="green"
-          icon="mdi-store"
-          title="Revenue"
-          value="$34,245"
-          sub-icon="mdi-calendar"
-          sub-text="Last 24 Hours"
+          icon="mdi-information-outline"
+          title="Max Accidents in 2014"
+          value="67250"
+          sub-icon="mdi-tag"
+          sub-text="State - Tamil Nadu"
         />
       </v-flex>
-      <v-flex
-        sm6
-        xs12
-        md6
-        lg3
-      >
+      <v-flex sm6 xs12 md6 lg4>
         <material-stats-card
           color="orange"
-          icon="mdi-content-copy"
-          title="Used Space"
-          value="49/50"
-          small-value="GB"
-          sub-icon="mdi-alert"
-          sub-icon-color="error"
-          sub-text="Get More Space..."
-          sub-text-color="text-primary"
+          icon="mdi-information-outline"
+          title="Max Accidents in 2015"
+          value="69059"
+          sub-icon="mdi-tag"
+          sub-text="State - Tamil Nadu"
         />
       </v-flex>
-      <v-flex
-        sm6
-        xs12
-        md6
-        lg3
-      >
+      <v-flex sm6 xs12 md6 lg4>
         <material-stats-card
           color="red"
+          icon="mdi-information-outline"
+          title="Max Accidents in 2016"
+          value="71431"
+          sub-icon="mdi-tag"
+          sub-text="State - Tamil Nadu"
+        />
+      </v-flex>
+      <!-- <v-flex sm6 xs12 md6 lg3>
+        <material-stats-card
+          color="info"
           icon="mdi-information-outline"
           title="Fixed Issues"
           value="75"
           sub-icon="mdi-tag"
-          sub-text="Tracked from Github"
+          sub-text="State - "
         />
-      </v-flex>
-      <v-flex
-        sm6
-        xs12
-        md6
-        lg3
-      >
-        <material-stats-card
-          color="info"
-          icon="mdi-twitter"
-          title="Followers"
-          value="+245"
-          sub-icon="mdi-update"
-          sub-text="Just Updated"
-        />
-      </v-flex>
-      <v-flex
-        md12
-        sm12
-        lg4
-      >
+      </v-flex> -->
+      <v-flex md12 sm12 lg4>
         <material-chart-card
           :data="dailySalesChart.data"
           :options="dailySalesChart.options"
@@ -81,32 +50,18 @@
         >
           <h4 class="title font-weight-light">Daily Sales</h4>
           <p class="category d-inline-flex font-weight-light">
-            <v-icon
-              color="green"
-              small
-            >
-              mdi-arrow-up
-            </v-icon>
+            <v-icon color="green" small>mdi-arrow-up</v-icon>
             <span class="green--text">55%</span>&nbsp;
             increase in today's sales
           </p>
 
           <template slot="actions">
-            <v-icon
-              class="mr-2"
-              small
-            >
-              mdi-clock-outline
-            </v-icon>
+            <v-icon class="mr-2" small>mdi-clock-outline</v-icon>
             <span class="caption grey--text font-weight-light">updated 4 minutes ago</span>
           </template>
         </material-chart-card>
       </v-flex>
-      <v-flex
-        md12
-        sm12
-        lg4
-      >
+      <v-flex md12 sm12 lg4>
         <material-chart-card
           :data="emailsSubscriptionChart.data"
           :options="emailsSubscriptionChart.options"
@@ -118,21 +73,12 @@
           <p class="category d-inline-flex font-weight-light">Last Campaign Performance</p>
 
           <template slot="actions">
-            <v-icon
-              class="mr-2"
-              small
-            >
-              mdi-clock-outline
-            </v-icon>
+            <v-icon class="mr-2" small>mdi-clock-outline</v-icon>
             <span class="caption grey--text font-weight-light">updated 10 minutes ago</span>
           </template>
         </material-chart-card>
       </v-flex>
-      <v-flex
-        md12
-        sm12
-        lg4
-      >
+      <v-flex md12 sm12 lg4>
         <material-chart-card
           :data="dataCompletedTasksChart.data"
           :options="dataCompletedTasksChart.options"
@@ -143,232 +89,41 @@
           <p class="category d-inline-flex font-weight-light">Last Last Campaign Performance</p>
 
           <template slot="actions">
-            <v-icon
-              class="mr-2"
-              small
-            >
-              mdi-clock-outline
-            </v-icon>
+            <v-icon class="mr-2" small>mdi-clock-outline</v-icon>
             <span class="caption grey--text font-weight-light">campaign sent 26 minutes ago</span>
           </template>
         </material-chart-card>
       </v-flex>
-      <v-flex
-        md12
-        lg6
-      >
-        <material-card
-          color="orange"
-          title="Employee Stats"
-          text="New employees on 15th September, 2016"
-        >
-          <v-data-table
-            :headers="headers"
-            :items="items"
-            hide-actions
-          >
-            <template
-              slot="headerCell"
-              slot-scope="{ header }"
-            >
-              <span
-                class="font-weight-light text-warning text--darken-3"
-                v-text="header.text"
-              />
+      <v-flex md12 lg12>
+        <material-card color="orange" title="State Wise Accidental Stats" text>
+          <v-data-table :headers="headers" :items="items" hide-actions>
+            <template slot="headerCell" slot-scope="{ header }">
+              <span class="font-weight-light text-warning text--darken-3" v-text="header.text"/>
             </template>
-            <template
-              slot="items"
-              slot-scope="{ index, item }"
-            >
+            <template slot="items" slot-scope="{ index, item }">
               <td>{{ index + 1 }}</td>
-              <td>{{ item.name }}</td>
-              <td class="text-xs-right">{{ item.salary }}</td>
-              <td class="text-xs-right">{{ item.country }}</td>
-              <td class="text-xs-right">{{ item.city }}</td>
+              <td>{{ item.name.split("\n")[0].split("   ")[1].trim() }}</td>
+              <td class="text-xs-right">{{ item["2014"].split("\n")[0].split("   ")[1].trim() }}</td>
+              <td class="text-xs-right">{{ item["2015"].split("\n")[0].split("   ")[1].trim() }}</td>
+              <td class="text-xs-right">{{ item["2016"].split("\n")[0].split("   ")[1].trim() }}</td>
             </template>
           </v-data-table>
         </material-card>
       </v-flex>
-      <v-flex
-        md12
-        lg6
-      >
-        <material-card
-          class="card-tabs"
-          color="green">
-          <v-flex
-            slot="header"
-          >
-            <v-tabs
-              v-model="tabs"
-              color="transparent"
-              slider-color="white"
-            >
-              <span
-                class="subheading font-weight-light mr-3"
-                style="align-self: center"
-              >Tasks:</span>
-              <v-tab class="mr-3">
-                <v-icon class="mr-2">mdi-bug</v-icon>
-                Bugs
-              </v-tab>
-              <v-tab class="mr-3">
-                <v-icon class="mr-2">mdi-code-tags</v-icon>
-                Website
-              </v-tab>
-              <v-tab>
-                <v-icon class="mr-2">mdi-cloud</v-icon>
-                Server
-              </v-tab>
-            </v-tabs>
-          </v-flex>
-
-          <v-tabs-items v-model="tabs">
-            <v-tab-item
-              v-for="n in 3"
-              :key="n"
-            >
-              <v-list three-line>
-                <v-list-tile @click="complete(0)">
-                  <v-list-tile-action>
-                    <v-checkbox
-                      :value="list[0]"
-                      color="green"
-                    />
-                  </v-list-tile-action>
-                  <v-list-tile-title>
-                    Sign contract for "What are conference organized afraid of?"
-                  </v-list-tile-title>
-                  <div class="d-flex">
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="success"
-                        icon
-                      >
-                        <v-icon color="primary">mdi-pencil</v-icon>
-                      </v-btn>
-                      <span>Edit</span>
-                    </v-tooltip>
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="danger"
-                        icon
-                      >
-                        <v-icon color="error">mdi-close</v-icon>
-                      </v-btn>
-                      <span>Close</span>
-                    </v-tooltip>
-
-                  </div>
-                </v-list-tile>
-                <v-divider/>
-                <v-list-tile @click="complete(1)">
-                  <v-list-tile-action>
-                    <v-checkbox
-                      :value="list[1]"
-                      color="success"
-                    />
-                  </v-list-tile-action>
-                  <v-list-tile-title>
-                    Lines From Great Russian Literature? Or E-mails From My Boss?
-                  </v-list-tile-title>
-                  <div class="d-flex">
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="success"
-                        icon
-                      >
-                        <v-icon color="primary">mdi-pencil</v-icon>
-                      </v-btn>
-                      <span>Edit</span>
-                    </v-tooltip>
-
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="danger"
-                        icon>
-                        <v-icon color="error">mdi-close</v-icon>
-                      </v-btn>
-                      <span>Close</span>
-                    </v-tooltip>
-                  </div>
-                </v-list-tile>
-                <v-divider/>
-                <v-list-tile @click="complete(2)">
-                  <v-list-tile-action>
-                    <v-checkbox
-                      :value="list[2]"
-                      color="success"
-                    />
-                  </v-list-tile-action>
-                  <v-list-tile-title>
-                    Flooded: One year later, assessing what was lost and what was found when a ravaging rain swept through metro Detroit
-                  </v-list-tile-title>
-                  <div class="d-flex">
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="success"
-                        icon
-                      >
-                        <v-icon color="primary">mdi-pencil</v-icon>
-                      </v-btn>
-                      <span>Edit</span>
-                    </v-tooltip>
-                    <v-tooltip
-                      top
-                      content-class="top">
-                      <v-btn
-                        slot="activator"
-                        class="v-btn--simple"
-                        color="danger"
-                        icon
-                      >
-                        <v-icon color="error">mdi-close</v-icon>
-                      </v-btn>
-                      <span>Close</span>
-                    </v-tooltip>
-
-                  </div>
-                </v-list-tile>
-              </v-list>
-            </v-tab-item>
-          </v-tabs-items>
-        </material-card>
-      </v-flex>
+      
     </v-layout>
   </v-container>
 </template>
 
 <script>
+const axios = require("axios");
 export default {
-  data () {
+  data() {
     return {
       dailySalesChart: {
         data: {
-          labels: ['M', 'T', 'W', 'T', 'F', 'S', 'S'],
-          series: [
-            [12, 17, 7, 17, 23, 18, 38]
-          ]
+          labels: ["M", "T", "W", "T", "F", "S", "S"],
+          series: [[12, 17, 7, 17, 23, 18, 38]]
         },
         options: {
           lineSmooth: this.$chartist.Interpolation.cardinal({
@@ -386,10 +141,8 @@ export default {
       },
       dataCompletedTasksChart: {
         data: {
-          labels: ['12am', '3pm', '6pm', '9pm', '12pm', '3am', '6am', '9am'],
-          series: [
-            [230, 750, 450, 300, 280, 240, 200, 190]
-          ]
+          labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
+          series: [[230, 750, 450, 300, 280, 240, 200, 190]]
         },
         options: {
           lineSmooth: this.$chartist.Interpolation.cardinal({
@@ -407,11 +160,21 @@ export default {
       },
       emailsSubscriptionChart: {
         data: {
-          labels: ['Ja', 'Fe', 'Ma', 'Ap', 'Mai', 'Ju', 'Jul', 'Au', 'Se', 'Oc', 'No', 'De'],
-          series: [
-            [542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]
-
-          ]
+          labels: [
+            "Ja",
+            "Fe",
+            "Ma",
+            "Ap",
+            "Mai",
+            "Ju",
+            "Jul",
+            "Au",
+            "Se",
+            "Oc",
+            "No",
+            "De"
+          ],
+          series: [[542, 443, 320, 780, 553, 453, 326, 434, 568, 610, 756, 895]]
         },
         options: {
           axisX: {
@@ -427,87 +190,96 @@ export default {
           }
         },
         responsiveOptions: [
-          ['screen and (max-width: 640px)', {
-            seriesBarDistance: 5,
-            axisX: {
-              labelInterpolationFnc: function (value) {
-                return value[0]
+          [
+            "screen and (max-width: 640px)",
+            {
+              seriesBarDistance: 5,
+              axisX: {
+                labelInterpolationFnc: function(value) {
+                  return value[0];
+                }
               }
             }
-          }]
+          ]
         ]
       },
       headers: [
         {
           sortable: false,
-          text: 'ID',
-          value: 'id'
+          text: "ID",
+          value: "id"
         },
         {
           sortable: false,
-          text: 'Name',
-          value: 'name'
+          text: "State",
+          value: "name"
         },
         {
           sortable: false,
-          text: 'Salary',
-          value: 'salary',
-          align: 'right'
+          text: "Accidents in 2014",
+          value: "2014",
+          align: "right"
         },
         {
           sortable: false,
-          text: 'Country',
-          value: 'country',
-          align: 'right'
+          text: "Accidents in 2015",
+          value: "2015",
+          align: "right"
         },
         {
           sortable: false,
-          text: 'City',
-          value: 'city',
-          align: 'right'
-        }
+          text: "Accidents in 2016",
+          value: "2016",
+          align: "right"
+        },
       ],
-      items: [
-        {
-          name: 'Dakota Rice',
-          country: 'Niger',
-          city: 'Oud-Tunrhout',
-          salary: '$35,738'
-        },
-        {
-          name: 'Minerva Hooper',
-          country: 'Curaçao',
-          city: 'Sinaai-Waas',
-          salary: '$23,738'
-        }, {
-          name: 'Sage Rodriguez',
-          country: 'Netherlands',
-          city: 'Overland Park',
-          salary: '$56,142'
-        }, {
-          name: 'Philip Chanley',
-          country: 'Korea, South',
-          city: 'Gloucester',
-          salary: '$38,735'
-        }, {
-          name: 'Doris Greene',
-          country: 'Malawi',
-          city: 'Feldkirchen in Kārnten',
-          salary: '$63,542'
-        }
-      ],
+      items: [],
+      max2014: {
+        value: '67250',
+        state: 'Tamil Nadu'
+      },
+      max2015: {
+        value: '69059',
+        state: 'Tamil Nadu'
+      },
+      max2016: {
+        value: '71431',
+        state: 'Tamil Nadu'
+      },
       tabs: 0,
       list: {
         0: false,
         1: false,
         2: false
       }
-    }
+    };
+  },
+  mounted() {
+    this.fetchData();
   },
   methods: {
-    complete (index) {
-      this.list[index] = !this.list[index]
+    complete(index) {
+      this.list[index] = !this.list[index];
+    },
+    fetchData() {
+      // Make a request for a user with a given ID
+      let vm = this;
+      axios
+        .get("http://localhost:9000/states")
+        .then(function(response) {
+          // handle success
+          vm.items = response.data;
+          vm.max2014 = 
+          console.log(response.data);
+        })
+        .catch(function(error) {
+          // handle error
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
     }
   }
-}
+};
 </script>
